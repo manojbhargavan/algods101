b882484fef2bd2397f352603aac06f110797db71
Manoj Bhargavan <manojbhargavan07@gmail.com>
2019-11-19 22:46:47 +0530

    Few changes

--- src/bigO/Program.cs
+++ src/bigO/Program.cs
@@ -8,13 +8,14 @@ namespace bigO
         static void Main(string[] args)
         {
             //1. O(n) Function
-            //Example1.FindingNemo(new string[] { "manoj", "nandan",
-            //"saranya", "jack", "nemo", "karimeen" });
-            //var startTime = DateTime.Now;
-            //var nemoArray = "nemo".GetEnumerablePrePopulated(100000);
-            //Example1.FindingNemo(nemoArray);
-            //System.Console.WriteLine($"Time taken to run: {(DateTime.Now - startTime).Humanize()}");
+            Example1.FindingNemo(new string[] { "manoj", "nandan",
+            "saranya", "jack", "nemo", "karimeen" });
+            var startTime = DateTime.Now;
+            var nemoArray = "nemo".GetEnumerablePrePopulated(100000);
+            Example1.FindingNemo(nemoArray);
+            System.Console.WriteLine($"Time taken to run: {(DateTime.Now - startTime).Humanize()}");
 
+            //2. O(n) example again
             var names = new string[] { "manoj", "nandan",
                 "saranya", "jack", "giant", "findme" };
             var itemsToFind = "findme";
@@ -30,6 +31,8 @@ namespace bigO
                 }
             }
             Console.WriteLine($"Steps taken {stepsTaken}");
+
+            //3. 
         }
     }
 }
